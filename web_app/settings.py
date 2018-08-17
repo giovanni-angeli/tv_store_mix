12b6cbfa6bd68bfd4f73b589529175096005984f
giovanni-angeli <giovanni.angeli6@gmail.com>
2018-08-17 18:09:49 +0200

    fix sqlite3 db path

--- web_app/settings.py
+++ web_app/settings.py
@@ -76,7 +76,8 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': "/opt/run/dj_mix.sqlite3",
+        'NAME': os.path.join(BASE_DIR, 'dj_mix.sqlite3')),
+        
     }
 }
 
